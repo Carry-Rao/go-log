37f547b7c42ab8f555e70e4cf5767b0145f812eb
GitHub Carry-Rao <GitHub carry3658@outlook.com>
2025-08-28 14:29:27 +0800

    v1.0.1

--- init.go
+++ init.go
@@ -1,22 +1,35 @@
 package log
 
-func InitDBMysql(dsn string) {
+func initDBMysql(dsn string) {
 	initErrorDB("mysql", dsn+".error")
 	initWarnDB("mysql", dsn+".warn")
 	initInfoDB("mysql", dsn+".info")
 	initDebugDB("mysql", dsn+".debug")
 }
 
-func InitDBPostgres(dsn string) {
+func initDBPostgres(dsn string) {
 	initErrorDB("postgres", dsn+".error")
 	initWarnDB("postgres", dsn+".warn")
 	initInfoDB("postgres", dsn+".info")
 	initDebugDB("postgres", dsn+".debug")
 }
 
-func InitDBSqlite(path string) {
+func initDBSqlite(path string) {
 	initErrorDB("sqlite3", path+"/error.db")
 	initWarnDB("sqlite3", path+"/warn.db")
 	initInfoDB("sqlite3", path+"/info.db")
 	initDebugDB("sqlite3", path+"/debug.db")
+}
+
+func InitDB(typ, path string) {
+	switch typ {
+	case "mysql":
+		initDBMysql(path)
+	case "postgres":
+		initDBPostgres(path)
+	case "sqlite3":
+		initDBSqlite(path)
+	default:
+		panic("unsupported database type")
+	}
 }
